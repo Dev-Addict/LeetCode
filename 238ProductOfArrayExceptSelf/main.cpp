55cf18b3ac395946e4cdc47d28cacfdbfe6dd0e9
Aria AzadiPour <aazadipour@pm.me>
2024-08-11 00:47:33 -0230

    238 - Product of Array Except Self

---
+++ 238ProductOfArrayExceptSelf/main.cpp
@@ -0,0 +1,36 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+  vector<int> productExceptSelf(vector<int> &nums) {
+    int productTotal = 1, zeroes = 0;
+
+    for (int &num : nums) {
+      if (num == 0)
+        zeroes++;
+      else
+        productTotal *= num;
+    }
+
+    if (zeroes >= 2)
+      return vector<int>(nums.size(), 0);
+    if (zeroes == 1) {
+      vector<int> result(nums.size(), 0);
+
+      for (int i = 0; i < nums.size(); i++)
+        if (nums[i] == 0) {
+          result[i] = productTotal;
+
+          return result;
+        }
+    }
+
+    vector<int> result(nums.size());
+
+    for (int i = 0; i < nums.size(); i++)
+      result[i] = productTotal / nums[i];
+
+    return result;
+  }
+};
