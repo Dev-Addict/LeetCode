874aeb1059c73add3b926f00e3fd0758fc7f4fb3
Aria AzadiPour <aazadipour@pm.me>
2024-08-31 20:31:00 -0230

    70 - Climbing Stairs

---
+++ 70ClimbingStairs/index.ts
@@ -0,0 +1,13 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+function climbStairs(n: number) {
+  const dp = new Array(n + 1);
+
+  dp[0] = 1;
+  dp[1] = 1;
+
+  for (let i = 2; i <= n; i++) {
+    dp[i] = dp[i - 1] + dp[i - 2];
+  }
+
+  return dp[n];
+}
