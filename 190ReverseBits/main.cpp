88ab33a3796da7568ad2d74c9a099a5298929a0e
Aria AzadiPour <aazadipour@pm.me>
2024-09-09 13:17:11 -0230

    190 - Reverse Bits

--- 190ReverseBits/main.cpp
+++ 190ReverseBits/main.cpp
@@ -1,15 +1,24 @@
+#include <cmath>
 #include <cstdint>
 using namespace std;
 
 class Solution {
 public:
   uint32_t reverseBits(uint32_t n) {
+    if (n == 0) {
+      return 0;
+    }
+
     uint32_t result = 0;
+    int length{0};
 
     while (n != 0) {
       result = result * 2 + n % 2;
       n /= 2;
+      length++;
     }
+
+    result = result * pow(2, 32 - length);
 
     return result;
   }
