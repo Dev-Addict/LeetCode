89521e98db08e025eb47977a0c60a78cf913103e
Aria AzadiPour <aazadipour@pm.me>
2025-01-31 20:43:20 -0330

    3151 - Special Array I

---
+++ 3151SpecialArrayI/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "C"
+
+func isArraySpecial(nums []int) bool {
+	for i := 0; i < len(nums)-1; i++ {
+		if nums[i]%2 == nums[i+1]%2 {
+			return false
+		}
+	}
+
+	return true
+}
