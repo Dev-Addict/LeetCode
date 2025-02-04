9056b83ff640abbe4be99738970fc63ae7744f78
Aria AzadiPour <aazadipour@pm.me>
2025-02-04 15:29:49 -0330

    made improvements

--- 89GrayCode/main.go
+++ 89GrayCode/main.go
@@ -1,6 +1,7 @@
 package main
 
 import "C"
+import "math"
 
 func grayCode(n int) []int {
 	if n == 0 {
@@ -8,19 +9,17 @@ func grayCode(n int) []int {
 	}
 
 	currentBase := 2
-	result := []int{0, 1}
+	result := make([]int, int(math.Pow(2.0, float64(n))))
+
+	result[0] = 0
+	result[1] = 1
 
 	for i := 1; i < n; i++ {
-		newResult := make([]int, currentBase*2)
-
-		copy(newResult[:], result[:])
-
 		for j := 0; j < currentBase; j++ {
-			newResult[currentBase+j] = newResult[currentBase-j-1] + currentBase
+			result[currentBase+j] = result[currentBase-j-1] + currentBase
 		}
 
 		currentBase *= 2
-		result = newResult
 	}
 
 	return result
